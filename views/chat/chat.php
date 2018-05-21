b34f29957b6b0f751070acff1cc75270bedfb39b
ak1987 <jazz1987@mail.ru>
2018-05-21 21:14:26 +0300

    removed dummy mesages

--- views/chat/chat.php
+++ views/chat/chat.php
@@ -8,50 +8,7 @@ ChatAsset::register($this);
 <div class="row">
     <div class="col-sm-12 col-md-10">
         <div id="chat-window">
-            <div id="chat-messages">
-                <div class="chat-message">
-                    <div class="chat-message-header">
-                        2018-01-01 16:22:22 | Вася:
-                    </div>
-                    <div class="chat-message-body">
-                        Привет
-                    </div>
-                </div>
-                <div class="chat-message chat-message-service">
-                    <div class="chat-message-header">
-                        <span class="label label-danger">Вася is now offline</span>
-                    </div>
-                </div>
-                <div class="chat-message">
-                    <div class="chat-message-header">
-                        2018-01-01 16:22:22 | Вася:
-                    </div>
-                    <div class="chat-message-body">
-                        Привет
-                    </div>
-                </div>
-                <div class="chat-message">
-                    <div class="chat-message-header">
-                        2018-01-01 16:22:22 | Вася:
-                    </div>
-                    <div class="chat-message-body">
-                        Привет
-                    </div>
-                </div>
-                <div class="chat-message chat-message-service">
-                    <div class="chat-message-header">
-                        <span class="label label-success">Вася is now online</span>
-                    </div>
-                </div>
-                <div class="chat-message">
-                    <div class="chat-message-header">
-                        2018-01-01 16:22:22 | Вася:
-                    </div>
-                    <div class="chat-message-body">
-                        Привет
-                    </div>
-                </div>
-            </div>
+            <div id="chat-messages"></div>
             <div id="chat-controls">
                 <input type="hidden" id="chat-id" value="<?= $chatId ?>">
                 <textarea id="chat-new-message" rows="10"></textarea>
