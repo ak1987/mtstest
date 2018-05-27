6a22f3f60f6d7c3f719a9f97149fb729b62b978b
ak1987 <jazz1987@mail.ru>
2018-05-27 22:53:48 +0300

    added CORS access for localhost

--- controllers/ChatController.php
+++ controllers/ChatController.php
@@ -12,12 +12,41 @@ use app\models\RegisterForm;
 
 class ChatController extends Controller
 {
+
     /**
-     * {@inheritdoc}
+     * List of allowed domains.
+     * Note: Restriction works only for AJAX (using CORS, is not secure).
+     *
+     * @return array List of domains, that can access to this API
+     */
+    public static function allowedDomains()
+    {
+        return [
+            // '*',                        // star allows all domains
+            'http://localhost:4200',
+        ];
+    }
+
+    /**
+     * @inheritdoc
      */
     public function behaviors()
     {
-        return [];
+        return array_merge(parent::behaviors(), [
+
+            // For cross-domain AJAX request
+            'corsFilter'  => [
+                'class' => \yii\filters\Cors::className(),
+                'cors'  => [
+                    // restrict access to domains:
+                    'Origin'                           => static::allowedDomains(),
+                    'Access-Control-Request-Method'    => ['POST','GET'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
+                ],
+            ],
+
+        ]);
     }
 
     /**
